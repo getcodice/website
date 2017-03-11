46be8acbc8f3f7f5cb8113d735688b8868d7f9bd
Sobak <msobaczewski@gmail.com>
2017-03-11 14:28:21 +0100

    Update GitHub URLs

--- config/app.php
+++ config/app.php
@@ -3,5 +3,5 @@ return [
     'analytics' => getenv('ANALYTICS'),
     'base_url' => getenv('BASE_URL'),
     'debug' => getenv('APP_DEBUG') === 'true', // fixme: find the way to cast properly
-    'github_url' => 'https://github.com/Sobak/Codice',
+    'github_url' => 'https://github.com/getcodice/codice',
 ];
